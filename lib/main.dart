266988ecbaa3a41ae5a7972cec306f0b800f55ff
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2024-03-07 14:29:43 +0700

    fix wrong landing page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:starwarsproj_flutter/pages/home_page.dart';
-import 'package:starwarsproj_flutter/pages/login_page.dart';
 
 void main() async {
   Logger.root.level = Level.ALL;
@@ -35,6 +34,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {    
-    return LoginPage();
+    return HomePage();
   }
 }
